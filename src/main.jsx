2eac9db77cc8dd39ced4c03118c4d5c42cbe211d
Shobun Kollied Anwar <shobunkollieda@gmail.com>
2026-06-10 09:47:12 +0700

    edit sandbox

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const loadMidtrans = () => {
+  const script = document.createElement('script')
+  script.src = 'https://app.sandbox.midtrans.com/snap/snap.js'
+  script.setAttribute(
+    'data-client-key',
+    import.meta.env.VITE_MIDTRANS_CLIENT_KEY
+  )
+  script.async = true
+  document.body.appendChild(script)
+}
+
+loadMidtrans()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
